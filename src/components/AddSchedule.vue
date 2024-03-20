d9da5251c6ca97d7e6c44055ab5802d2c0010339
钟纯帅哥123456 <517048575@qq.com>
2024-03-20 16:32:59 +0800

    电影排期方案一

--- src/components/AddSchedule.vue
+++ src/components/AddSchedule.vue
@@ -1,16 +1,15 @@
 <template>
     <el-table :data="tableData" style="width: 100%" max-height="250">
-        <el-table-column fixed prop="date" label="Date" width="150" />
-        <el-table-column prop="name" label="Name" width="120" />
-        <el-table-column prop="state" label="State" width="120" />
-        <el-table-column prop="city" label="City" width="120" />
-        <el-table-column prop="address" label="Address" width="600" />
-        <el-table-column prop="zip" label="Zip" width="120" />
+        <el-table-column prop="MovieName" label="MovieName" width="150" />
+        <el-table-column prop="ReleaseTime" label="ReleaseTime" width="120" />
+        <el-table-column prop="StartTime" label="StartTime" width="120" />
+        <el-table-column prop="EndTime" label="EndTime" width="120" />
+        <el-table-column prop="Room" label="Room" width="600" />
         <el-table-column fixed="right" label="Operations" width="120">
             <template #default="scope">
-                <!-- <el-button link type="primary" size="large" @click.prevent="openEditDialog(scope.$index)">
+                <el-button link type="primary" size="large" @click.prevent="openEditDialog(scope.row)">
                     编辑
-                </el-button> -->
+                </el-button>
                 <el-button link type="primary" size="large" @click.prevent="openConfirmDialog(scope.$index)">
                     删除
                 </el-button>
@@ -27,28 +26,25 @@ export default {
         return {
             tableData: [
                 {
-                    date: '2016-05-01',
-                    name: 'Tom',
-                    state: 'California',
-                    city: 'Los Angeles',
-                    address: 'No. 189, Grove St, Los Angeles',
-                    zip: 'CA 90036',
+                    MovieName: '热辣滚烫',
+                    ReleaseTime: '2024-3-30',
+                    StartTime: '20:30:00',
+                    EndTime: '22:30:00',
+                    Room: '1',
                 },
                 {
-                    date: '2016-05-02',
-                    name: 'Tom',
-                    state: 'California',
-                    city: 'Los Angeles',
-                    address: 'No. 189, Grove St, Los Angeles',
-                    zip: 'CA 90036',
+                    MovieName: '飞驰人生2',
+                    ReleaseTime: '2024-3-30',
+                    StartTime: '20:30:00',
+                    EndTime: '22:30:00',
+                    Room: '1',
                 },
                 {
-                    date: '2016-05-03',
-                    name: 'Tom',
-                    state: 'California',
-                    city: 'Los Angeles',
-                    address: 'No. 189, Grove St, Los Angeles',
-                    zip: 'CA 90036',
+                    MovieName: '功夫熊猫4',
+                    ReleaseTime: '2024-3-30',
+                    StartTime: '20:30:00',
+                    EndTime: '22:30:00',
+                    Room: '1',
                 }
             ]
         };
@@ -81,110 +77,85 @@ export default {
                     });
                 });
         },
+
         onAddItem() {
-            this.$prompt('请输入日期', '添加电影', {
+            this.$prompt('请输入电影名称', '添加电影', {
                 confirmButtonText: '确认',
                 cancelButtonText: '取消',
                 inputPattern: /^(.+)$/,
                 inputErrorMessage: '请输入有效的值',
-                inputPlaceholder: '请输入日期',
+                inputPlaceholder: '请输入电影名称',
             })
-                .then(({ value: date }) => {
-                    this.$prompt('请输入名称', '添加电影', {
+                .then(({ value: ReleaseTime }) => {
+                    this.$prompt('请输入上映时间', '添加电影', {
                         confirmButtonText: '确认',
                         cancelButtonText: '取消',
                         inputPattern: /^(.+)$/,
                         inputErrorMessage: '请输入有效的值',
-                        inputPlaceholder: '请输入名称',
+                        inputPlaceholder: '请输入上映时间',
                     })
-                        .then(({ value: name }) => {
-                            this.$prompt('请输入州', '添加电影', {
+                        .then(({ value: StartTime }) => {
+                            this.$prompt('请输入开始时间', '添加电影', {
                                 confirmButtonText: '确认',
                                 cancelButtonText: '取消',
                                 inputPattern: /^(.+)$/,
                                 inputErrorMessage: '请输入有效的值',
-                                inputPlaceholder: '请输入州',
+                                inputPlaceholder: '请输入开始时间',
                             })
-                                .then(({ value: state }) => {
-                                    this.$prompt('请输入城市', '添加电影', {
+                                .then(({ value: EndTime }) => {
+                                    this.$prompt('请输入结束时间', '添加电影', {
                                         confirmButtonText: '确认',
                                         cancelButtonText: '取消',
                                         inputPattern: /^(.+)$/,
                                         inputErrorMessage: '请输入有效的值',
-                                        inputPlaceholder: '请输入城市',
+                                        inputPlaceholder: '请输入结束时间',
                                     })
-                                        .then(({ value: city }) => {
-                                            this.$prompt('请输入地址', '添加电影', {
+                                        .then(({ value: Room }) => {
+                                            this.$prompt('请输入影厅', '添加电影', {
                                                 confirmButtonText: '确认',
                                                 cancelButtonText: '取消',
                                                 inputPattern: /^(.+)$/,
                                                 inputErrorMessage: '请输入有效的值',
-                                                inputPlaceholder: '请输入地址',
+                                                inputPlaceholder: '请输入影厅',
                                             })
-                                                .then(({ value: address }) => {
-                                                    this.$prompt('请输入邮编', '添加电影', {
-                                                        confirmButtonText: '确认',
-                                                        cancelButtonText: '取消',
-                                                        inputPattern: /^(.+)$/,
-                                                        inputErrorMessage: '请输入有效的值',
-                                                        inputPlaceholder: '请输入邮编',
-                                                    })
-                                                        .then(({ value: zip }) => {
-                                                            this.tableData.push({
-                                                                date,
-                                                                name,
-                                                                state,
-                                                                city,
-                                                                address,
-                                                                zip,
-                                                            });
-                                                            this.$message({
-                                                                type: 'success',
-                                                                message: '添加成功',
-                                                            });
-                                                        })
-                                                        .catch(() => {
-                                                            this.$message({
-                                                                type: 'info',
-                                                                message: '取消操作',
-                                                            });
-                                                        });
-                                                })
                                                 .catch(() => {
-                                                    this.$message({
+                                                    ElMessage({
                                                         type: 'info',
                                                         message: '取消操作',
                                                     });
                                                 });
                                         })
                                         .catch(() => {
-                                            this.$message({
+                                            ElMessage({
                                                 type: 'info',
                                                 message: '取消操作',
                                             });
                                         });
                                 })
                                 .catch(() => {
-                                    this.$message({
+                                    ElMessage({
                                         type: 'info',
                                         message: '取消操作',
                                     });
                                 });
                         })
                         .catch(() => {
-                            this.$message({
+                            ElMessage({
                                 type: 'info',
                                 message: '取消操作',
                             });
                         });
                 })
                 .catch(() => {
-                    this.$message({
+                    ElMessage({
                         type: 'info',
                         message: '取消操作',
                     });
                 });
         },
+        openEditDialog(row) {
+            console.log('编辑按钮被点击，打开编辑对话框，允许用户编辑数据', row)
+        }
     },
 
 }
