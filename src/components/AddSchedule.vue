2cdf57ca723febe91511f7cb705c7d991480c4a2
钟纯帅哥123456 <517048575@qq.com>
2024-03-20 16:04:44 +0800

    电影排期完成

--- src/components/AddSchedule.vue
+++ src/components/AddSchedule.vue
@@ -1,9 +1,191 @@
 <template>
-    <h3>这是添加排期</h3>
+    <el-table :data="tableData" style="width: 100%" max-height="250">
+        <el-table-column fixed prop="date" label="Date" width="150" />
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column prop="state" label="State" width="120" />
+        <el-table-column prop="city" label="City" width="120" />
+        <el-table-column prop="address" label="Address" width="600" />
+        <el-table-column prop="zip" label="Zip" width="120" />
+        <el-table-column fixed="right" label="Operations" width="120">
+            <template #default="scope">
+                <!-- <el-button link type="primary" size="large" @click.prevent="openEditDialog(scope.$index)">
+                    编辑
+                </el-button> -->
+                <el-button link type="primary" size="large" @click.prevent="openConfirmDialog(scope.$index)">
+                    删除
+                </el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+    <el-button class="mt-4" style="width: 100%;color: red;" @click="onAddItem">添加电影</el-button>
 </template>
 
 <script>
+import { ElMessageBox, ElMessage } from 'element-plus';
 export default {
+    data() {
+        return {
+            tableData: [
+                {
+                    date: '2016-05-01',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                },
+                {
+                    date: '2016-05-02',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                },
+                {
+                    date: '2016-05-03',
+                    name: 'Tom',
+                    state: 'California',
+                    city: 'Los Angeles',
+                    address: 'No. 189, Grove St, Los Angeles',
+                    zip: 'CA 90036',
+                }
+            ]
+        };
+    },
+    methods: {
+        deleteRow(index) {
+            this.tableData.splice(index, 1);
+        },
+        openConfirmDialog(index) {
+            ElMessageBox.confirm(
+                '确定要删除这行数据吗？',
+                '警告',
+                {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning',
+                }
+            )
+                .then(() => {
+                    this.deleteRow(index);
+                    ElMessage({
+                        type: 'success',
+                        message: '删除成功',
+                    });
+                })
+                .catch(() => {
+                    ElMessage({
+                        type: 'info',
+                        message: '取消删除',
+                    });
+                });
+        },
+        onAddItem() {
+            this.$prompt('请输入日期', '添加电影', {
+                confirmButtonText: '确认',
+                cancelButtonText: '取消',
+                inputPattern: /^(.+)$/,
+                inputErrorMessage: '请输入有效的值',
+                inputPlaceholder: '请输入日期',
+            })
+                .then(({ value: date }) => {
+                    this.$prompt('请输入名称', '添加电影', {
+                        confirmButtonText: '确认',
+                        cancelButtonText: '取消',
+                        inputPattern: /^(.+)$/,
+                        inputErrorMessage: '请输入有效的值',
+                        inputPlaceholder: '请输入名称',
+                    })
+                        .then(({ value: name }) => {
+                            this.$prompt('请输入州', '添加电影', {
+                                confirmButtonText: '确认',
+                                cancelButtonText: '取消',
+                                inputPattern: /^(.+)$/,
+                                inputErrorMessage: '请输入有效的值',
+                                inputPlaceholder: '请输入州',
+                            })
+                                .then(({ value: state }) => {
+                                    this.$prompt('请输入城市', '添加电影', {
+                                        confirmButtonText: '确认',
+                                        cancelButtonText: '取消',
+                                        inputPattern: /^(.+)$/,
+                                        inputErrorMessage: '请输入有效的值',
+                                        inputPlaceholder: '请输入城市',
+                                    })
+                                        .then(({ value: city }) => {
+                                            this.$prompt('请输入地址', '添加电影', {
+                                                confirmButtonText: '确认',
+                                                cancelButtonText: '取消',
+                                                inputPattern: /^(.+)$/,
+                                                inputErrorMessage: '请输入有效的值',
+                                                inputPlaceholder: '请输入地址',
+                                            })
+                                                .then(({ value: address }) => {
+                                                    this.$prompt('请输入邮编', '添加电影', {
+                                                        confirmButtonText: '确认',
+                                                        cancelButtonText: '取消',
+                                                        inputPattern: /^(.+)$/,
+                                                        inputErrorMessage: '请输入有效的值',
+                                                        inputPlaceholder: '请输入邮编',
+                                                    })
+                                                        .then(({ value: zip }) => {
+                                                            this.tableData.push({
+                                                                date,
+                                                                name,
+                                                                state,
+                                                                city,
+                                                                address,
+                                                                zip,
+                                                            });
+                                                            this.$message({
+                                                                type: 'success',
+                                                                message: '添加成功',
+                                                            });
+                                                        })
+                                                        .catch(() => {
+                                                            this.$message({
+                                                                type: 'info',
+                                                                message: '取消操作',
+                                                            });
+                                                        });
+                                                })
+                                                .catch(() => {
+                                                    this.$message({
+                                                        type: 'info',
+                                                        message: '取消操作',
+                                                    });
+                                                });
+                                        })
+                                        .catch(() => {
+                                            this.$message({
+                                                type: 'info',
+                                                message: '取消操作',
+                                            });
+                                        });
+                                })
+                                .catch(() => {
+                                    this.$message({
+                                        type: 'info',
+                                        message: '取消操作',
+                                    });
+                                });
+                        })
+                        .catch(() => {
+                            this.$message({
+                                type: 'info',
+                                message: '取消操作',
+                            });
+                        });
+                })
+                .catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消操作',
+                    });
+                });
+        },
+    },
 
 }
 </script>
--- src/components/ModifyPassword.vue
+++ src/components/ModifyPassword.vue
@@ -1,14 +1,10 @@
 <template>
-    <div class="container">
+    <div class="centered-container">
         <p>旧密码：</p>
         <el-input v-model="OldPassword" style="width: 240px" type="password" placeholder="请输入旧密码" show-password />
         <p>新密码：</p>
         <el-input v-model="NewPassword" style="width: 240px" type="password" placeholder="请输入新密码" show-password />
-        <br>
-        <br>
-        <br>
-        <br>
-        <el-button type="primary" @click="getInput">确认修改</el-button>
+        <button>确认修改</button>
     </div>
 </template>
 
@@ -21,13 +17,38 @@ export default {
         }
     },
     methods: {
-        getInput() {
-            console.log(this.OldPassword);
-            console.log(this.NewPassword);
-
-        }
     }
 }
 </script>
 
-<style></style>
+<style>
+.centered-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+
+}
+
+.centered-container p {
+    margin: 10px;
+}
+
+.centered-container button {
+    background-color: #409EFF;
+    color: #FFF0FF;
+    margin-top: 30px;
+    width: 100px;
+    height: 30px;
+    cursor: pointer;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.centered-container button:hover {
+    background-color: #79BBFF;
+}
+</style>
